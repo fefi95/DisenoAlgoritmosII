a9875594820ca1d0acd7c3b551f9ea85ad54b31d
Erick <ericksilvarodriguez@gmail.com>
2017-07-01 15:22:08 -0400

    mejorado scatter por eficiencia

--- APC/scatter.cpp
+++ APC/scatter.cpp
@@ -18,7 +18,7 @@
 #include <random>
 #include "seed.hpp"
 #include <utility> //pairs
-
+#include <cstdio>
 using namespace std;
 
 /**
@@ -53,20 +53,25 @@ APC_Instance scatter(APC_Instance initial,
     for (int i = 1; i < popSize; i++){
         referenceSet[i] = APC_Instance(length);
     }  
-
     vector<APC_Instance> children;
-    int combinationsListIndex = 0;
-    bool changeInRefSet = 0;
-    int noChange = 0;
-    for (int it = 0 ; it < maxIterations ; it++){
-        //generate the combinationsList
-        combinationsListIndex = 0;
-        for (int i = 0 ; i < popSize; i++){
-            combinationsList[combinationsListIndex] = make_pair(referenceSet[i].evaluate(trainingSet,testSet),
-                                                                referenceSet[i]);
-            combinationsListIndex++;
-        }
 
+    int combinationsListIndex = 0;
+    double bestSolution = initial.evaluate(trainingSet,testSet);
+    int iterationsWithoutChange = 0;
+    int numberOfGoodSolutions = popSize/2;
+    int numberOfRandomSolutions = popSize - numberOfGoodSolutions;
+
+    //Initialize the first positions of the combinationList
+    for (int i = 0 ; i < popSize; i++){
+        combinationsList[combinationsListIndex] = make_pair(referenceSet[i].evaluate(trainingSet,testSet),
+                                                            referenceSet[i]);
+        combinationsListIndex++;
+    }
+
+    for (int it = 0 ; it < maxIterations ; it++){
+        
+        //generate the combinationsList
+        combinationsListIndex = popSize;
         for (int i = 0; i < popSize; i++){
             for (int j = i+1; j < popSize;j++){
                 children = referenceSet[i].blendAlphaCrossover(referenceSet[j]);
@@ -74,29 +79,35 @@ APC_Instance scatter(APC_Instance initial,
                     combinationsList[combinationsListIndex] = make_pair(child.evaluate(trainingSet,testSet),
                                                     child);
                     combinationsListIndex++;
+
                 }
             }
         }
 
         sort(combinationsList.begin(),combinationsList.end());
-        //Check for changes in ref set
-        changeInRefSet = 0;
-        for (int i = 0 ; i < popSize; i++){
-            if (referenceSet[i] != combinationsList[i].second){
-                changeInRefSet = 1;
-                referenceSet[i] = combinationsList[i].second;
-            }
-        }
 
-        if (!changeInRefSet){
-            noChange++;
+        //Check for changes in best solution
+        if (combinationsList[0].first > bestSolution){
+            bestSolution = combinationsList[0].first;
+            iterationsWithoutChange = 0;
         }
         else{
-            noChange = 0;
+            iterationsWithoutChange++;
         }
 
-        if (noChange > maxIterationsWithoutChange){
+        //if we're above the maximum iterations without an improvement
+        // end the loops.
+        if (iterationsWithoutChange > maxIterationsWithoutChange){
             break;
+        }
+
+        // Refresh the referenceSet
+        for (int i = 0; i < numberOfGoodSolutions; i++){
+            referenceSet[i] = combinationsList[i].second;
+        }
+        // Generate random solutions to "spice it up"
+        for (int i = numberOfGoodSolutions ; i < numberOfRandomSolutions;i++){
+            referenceSet[i] = APC_Instance(length);
         }
     }
 
